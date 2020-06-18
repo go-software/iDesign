fa01438059277f55af796809afdb461a6d63c920
go-software <66168786+go-software@users.noreply.github.com>
2020-06-19 00:24:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './images/i-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="app-logo" alt="logo" />            
+      </header>
+    </div>
+  );
+}
+
+export default App;
